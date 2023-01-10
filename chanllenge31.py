ef68b022358238076f164201d0c05e48c6963490
jmcmg1231 <jmcmg1231@gmail.com>
2023-01-10 18:40:37 +0000

    manipulating lists

---
+++ chanllenge31.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+wordbank = ["indentation", "space"]
+tlgstudents= ['Albert', 'Anthony', 'Brenden', 'Craig', 'Deja', 'Elihu', 'Eric', 'Giovanni', 'James', 'Joshua', 'Maria', 'Mohamed', 'PJ', 'Philip', 'Sagan', 'Suchit', 'Meka', 'Trey', 'Winton', 'Xiuxiang', 'Yaping']
+wordbank.append(4)
+print(wordbank)
+num = input("Please enter a number between 0 and 20") 
+Please enter a number between 0 and 2010
+num = int(input("Please a number between 0 and 20")
+num = int(input("Please enter a number between 0 and 20"))
+student_name = tlgstudents[num]
+print(student_name + " always uses " +str(wordbank[2]) + " " +str(wordbank[1]) + " to indent.") 
+     
