87305635eb866db61176502b8539050f06b0eab4
jmcmg1231 <jmcmg1231@gmail.com>
2023-01-12 22:02:35 +0000

    learning to parse log files

---
+++ attemptlogin/keystone_counter.py
@@ -0,0 +1,12 @@
+loginfail = 0 
+# parse keystone.common.wsgi and return number of failed login attempts
+keystone_file = open("/home/student/mycode/attemptlogin/keystone.common.wsgi","r")
+# turn the file into a list of lines in memory
+keystone_file_lines=keystone_file.readlines()
+# loop over the list of lines
+for line in keystone_file_lines:
+    # if this 'fail pattern' appears in the line...
+    if "- - - - -] Authorization failed" in line:
+        loginfail += 1 # this is the same as loginfail = loginfail + 1
+        print("The number of failed log in attempts is", loginfail)
+keystone_file.close() # close the open file
