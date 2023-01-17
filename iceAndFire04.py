20a1cbe3d684fd2eeafdf0de5454b2fc01b35d88
jmcmg1231 <jmcmg1231@gmail.com>
2023-01-17 22:47:25 +0000

    Exploring how to work with large data sets

---
+++ iceAndFire04.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""Alta3 Research - Exploring OpenAPIs with requests"""
+# documentation for this API is at
+# https://anapioficeandfire.com/Documentation
+
+import requests
+import pprint
+
+AOIF_CHAR = "https://www.anapioficeandfire.com/api/characters/"
+
+def name_finder(got_list):
+    names = []  # list to return back of decoded names
+    for x in got_list:
+        # send HTTP GET to one of the entries within the list
+        r = requests.get(x)
+        decodedjson = r.json() # decode the JSON on the response
+        names.append(decodedjson.get("name"))  # this returns the housename and adds it to our list
+    return names  # when operation is over, send it back
+
+def main():
+        ## Ask user for input
+        got_charToLookup = input("Pick a number between 1 and 1000 to return info on a GoT character! " )
+
+        ## Send HTTPS GET to the API of ICE and Fire character resource
+        gotresp = requests.get(AOIF_CHAR + got_charToLookup)
+
+        ## Decode the response
+        got_dj = gotresp.json()
+        pprint.pprint(got_dj)
+
+if __name__ == "__main__":
+        main()
+
